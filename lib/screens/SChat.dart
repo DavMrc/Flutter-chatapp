5edb9c752227f638a0c81dfc64fefa899d150f95
Davide Win10 <marcandavi@gmail.com>
2020-05-13 23:54:09 +0200

    added firebase messaging (push notifications) support
    + SChat.initState contains the methods to handle the push notifications
    + edited default emulator

--- lib/screens/SChat.dart
+++ lib/screens/SChat.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import '../widgets/WMessages.dart';
 import '../widgets/WNewMsg.dart';
 
 
-class SChat extends StatelessWidget {
-  static const routeName = "/chat";
-  static const msgsCollection = 'chats';
+class SChat extends StatefulWidget {
+  @override
+  _SChatState createState() => _SChatState();
+}
+
+class _SChatState extends State<SChat> {
+
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure(
+      onMessage: (msg){   // if app is on foreground
+        print(msg);
+        return;
+      },
+      onLaunch: (msg){    // if app was totally closed
+        print(msg);
+        return;
+      },
+      onResume: (msg){    // if app was on background
+        print(msg);
+        return;
+      },
+    );
+    
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
